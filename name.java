99d4b1f76b8a3c1552f3c7bd45b2a22b7f07b5ed
Ahilham <126547692+Ahilham@users.noreply.github.com>
2023-02-28 21:49:42 +0800

    Add files via upload

---
+++ name.java
@@ -0,0 +1,31 @@
+
+public class name {
+	private String fName;
+	private String mName;
+	private String lName;
+	
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	
+	public String getfName() {
+		return fName;
+	}
+	
+	public void setmName(String mName) {
+		this.mName=mName;
+	}
+	
+	public String getmName() {
+		return mName;
+	}
+	
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	
+	public String getlName() {
+		return lName;
+	}
+
+}
